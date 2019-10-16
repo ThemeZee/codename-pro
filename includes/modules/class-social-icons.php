36c146d0cfa72785a560b75ef7f8324e1fc3ea20
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-10-16 14:32:39 +0200

    added icons to social navigation menus

--- includes/modules/class-social-icons.php
+++ includes/modules/class-social-icons.php
@@ -28,7 +28,29 @@ class Codename_Pro_Social_Icons {
 		}
 
 		// Display Social Icons in Header.
-		//add_action( 'codename_before_header', array( __CLASS__, 'display_social_icons' ), 20 );
+		add_action( 'codename_before_header', array( __CLASS__, 'header_social_icons' ), 20 );
+
+		// Replace menu links with social icons.
+		add_filter( 'walker_nav_menu_start_el', array( __CLASS__, 'social_icons_menu_walker' ), 10, 4 );
+	}
+
+	/**
+	 * Displays social icons menu in header area
+	 *
+	 * @return void
+	 */
+	static function header_social_icons() {
+		// Check if there is a social menu.
+		if ( has_nav_menu( 'social-header' ) ) : ?>
+
+			<div id="header-social-icons" class="header-social-menu donovan-social-menu clearfix">
+
+				<?php self::display_social_icons_menu( 'social-header' ); ?>
+
+			</div>
+
+			<?php
+		endif;
 	}
 
 	/**
@@ -36,8 +58,122 @@ class Codename_Pro_Social_Icons {
 	 *
 	 * @return void
 	 */
-	static function display_social_icons() {
+	static function display_social_icons_menu( $menu ) {
+		wp_nav_menu( array(
+			'theme_location' => $menu,
+			'container'      => false,
+			'menu_class'     => $menu . '-menu social-icons-menu',
+			'echo'           => true,
+			'fallback_cb'    => '',
+			'link_before'    => '<span class = "screen-reader-text">',
+			'link_after'     => '</span>',
+			'depth'          => 1,
+		) );
+	}
 
+	/**
+	 * Return SVG markup for social icons.
+	 *
+	 * @param string $icon SVG icon id.
+	 * @return string $svg SVG markup.
+	 */
+	function get_social_svg( $icon = null ) {
+		// Return early if no icon was defined.
+		if ( empty( $icon ) ) {
+			return;
+		}
+
+		// Create SVG markup.
+		$svg  = '<svg class="icon icon-' . esc_attr( $icon ) . '" aria-hidden="true" role="img">';
+		$svg .= ' <use xlink:href="' . CODENAME_PRO_PLUGIN_URL . 'assets/icons/social-icons.svg#icon-' . esc_html( $icon ) . '"></use> ';
+		$svg .= '</svg>';
+
+		return $svg;
+	}
+
+	/**
+	 * Display SVG icons in social links menu.
+	 *
+	 * @param  string  $item_output The menu item output.
+	 * @param  WP_Post $item        Menu item object.
+	 * @param  int     $depth       Depth of the menu.
+	 * @param  array   $args        wp_nav_menu() arguments.
+	 * @return string  $item_output The menu item output with social icon.
+	 */
+	function social_icons_menu_walker( $item_output, $item, $depth, $args ) {
+
+		// Get supported social icons.
+		$social_icons = self::supported_social_icons();
+
+		// Change SVG icon inside social links menu if there is supported URL.
+		if ( 'social-top' === $args->theme_location || 'social-header' === $args->theme_location || 'social-footer' === $args->theme_location ) {
+			$icon = 'star';
+			foreach ( $social_icons as $attr => $value ) {
+				if ( false !== strpos( $item_output, $attr ) ) {
+					$icon = esc_attr( $value );
+				}
+			}
+			$item_output = str_replace( $args->link_after, '</span>' . self::get_social_svg( $icon ), $item_output );
+		}
+
+		return $item_output;
+	}
+
+	/**
+	 * Returns an array of supported social links (URL and icon name).
+	 *
+	 * @return array $social_links_icons
+	 */
+	function supported_social_icons() {
+		// Supported social links icons.
+		$supported_social_icons = array(
+			'500px.com'       => '500px',
+			'amazon'          => 'amazon',
+			'apple'           => 'apple',
+			'bandcamp.com'    => 'bandcamp',
+			'behance.net'     => 'behance',
+			'bitbucket.org'   => 'bitbucket',
+			'codepen.io'      => 'codepen',
+			'deviantart.com'  => 'deviantart',
+			'digg.com'        => 'digg',
+			'dribbble.com'    => 'dribbble',
+			'dropbox.com'     => 'dropbox',
+			'etsy.com'        => 'etsy',
+			'facebook.com'    => 'facebook',
+			'feed'            => 'feed',
+			'flickr.com'      => 'flickr',
+			'foursquare.com'  => 'foursquare',
+			'plus.google.com' => 'google-plus',
+			'github.com'      => 'github',
+			'instagram.com'   => 'instagram',
+			'linkedin.com'    => 'linkedin',
+			'mailto:'         => 'envelope-o',
+			'medium.com'      => 'medium',
+			'meetup.com'      => 'meetup',
+			'pinterest.com'   => 'pinterest-p',
+			'getpocket.com'   => 'get-pocket',
+			'reddit.com'      => 'reddit-alien',
+			'skype.com'       => 'skype',
+			'skype:'          => 'skype',
+			'slideshare.net'  => 'slideshare',
+			'snapchat.com'    => 'snapchat-ghost',
+			'soundcloud.com'  => 'soundcloud',
+			'spotify.com'     => 'spotify',
+			'stumbleupon.com' => 'stumbleupon',
+			'tumblr.com'      => 'tumblr',
+			'twitch.tv'       => 'twitch',
+			'twitter.com'     => 'twitter',
+			'vimeo.com'       => 'vimeo',
+			'vine.co'         => 'vine',
+			'vk.com'          => 'vk',
+			'wordpress.org'   => 'wordpress',
+			'wordpress.com'   => 'wordpress',
+			'xing.com'        => 'xing',
+			'yelp.com'        => 'yelp',
+			'youtube.com'     => 'youtube',
+		);
+
+		return $supported_social_icons;
 	}
 
 	/**
